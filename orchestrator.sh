b09bd8b677ef6afda6813f81c4b562000241b0d0
emanuele-artioli <emanuele.artioli@aau.at>
2024-06-05 00:07:39 +0200

    improved split_video_into_scenes.py and fixed resize_video function

--- orchestrator.sh
+++ orchestrator.sh
@@ -19,7 +19,7 @@ export bitrate=${12}
 for file in "videos"/*.{flv,mp4,mov,mkv,avi}; do
     if [[ -f "$file" ]]; then
         echo "Detected $file not split into scenes. Splitting..."
-        python split_video_into_scenes.py $file 45 10
+        python split_video_into_scenes.py $file 0.5 10
     fi
 done
 
@@ -34,17 +34,18 @@ resize_video() {
     local output_dir=$2
     local resolution=$3
     
-    # Check if the output file already exists
+    # Check if the output directory already exists, if not create it
     if [[ -f "$output_dir" ]]; then
         echo "$output_dir already exists"
         return 1
+    else
+        mkdir $output_dir
     fi
 
-    ffmpeg -i "$input_file" -an -vf scale="$resolution":force_original_aspect_ratio=increase,crop="$resolution" \
-           -c:v libx265 -crf 0 -pix_fmt yuv420p "$output_dir/frame_%04d.png"
+    ffmpeg -i "$input_file" -vf scale="$resolution":force_original_aspect_ratio=increase,crop="$resolution" "$output_dir/frame_%04d.png"
 }
 # resize scene based on experiment resolution, save into 
-resize_video videos/$1/scene_$2.mp4 videos/$1/scene_$2/"$3:$4"/original "$3:$4"
+resize_video "videos/$1/scene_$2.mp4" "videos/$1/scene_$2/$3:$4/original" "$3:$4"
 
 frames_into_video() {
     local input_dir="$1"
@@ -65,80 +66,80 @@ frames_into_video() {
 
 frames_into_video videos/$1/scene_$2/"$3:$4"/original videos/$1/scene_$2/"$3:$4"/original.mp4 $5 ${12}
 
-# run server script
-python server.py 
+# # run server script
+# python server.py 
 
-# move shrunk and masks into experiment folder
-mv -f "videos/$1/scene_$2/"$3:$4"/shrunk/" "videos/$1/scene_$2/"$3:$4"/$experiment_name/shrunk"
-mv -f "videos/$1/scene_$2/"$3:$4"/masks/" "videos/$1/scene_$2/"$3:$4"/$experiment_name/masks"
+# # move shrunk and masks into experiment folder
+# mv -f "videos/$1/scene_$2/"$3:$4"/shrunk/" "videos/$1/scene_$2/"$3:$4"/$experiment_name/shrunk"
+# mv -f "videos/$1/scene_$2/"$3:$4"/masks/" "videos/$1/scene_$2/"$3:$4"/$experiment_name/masks"
 
-# get shrunk video from frames
-frames_into_video "videos/$1/scene_$2/"$3:$4"/$experiment_name/shrunk" "videos/$1/scene_$2/"$3:$4"/$experiment_name/shrunk.mp4" $5 ${12}
+# # get shrunk video from frames
+# frames_into_video "videos/$1/scene_$2/"$3:$4"/$experiment_name/shrunk" "videos/$1/scene_$2/"$3:$4"/$experiment_name/shrunk.mp4" $5 ${12}
 
-# run client script
-python client.py
+# # run client script
+# python client.py
 
-# get stretched video from frames
-frames_into_video "videos/$1/scene_$2/"$3:$4"/$experiment_name/stretched" "videos/$1/scene_$2/"$3:$4"/$experiment_name/stretched.mp4" $5 ${12}
+# # get stretched video from frames
+# frames_into_video "videos/$1/scene_$2/"$3:$4"/$experiment_name/stretched" "videos/$1/scene_$2/"$3:$4"/$experiment_name/stretched.mp4" $5 ${12}
 
-# inpaint video
-cd
-stretched_video_path="embrace/videos/$1/scene_$2/"$3:$4"/$experiment_name/stretched.mp4"
-mask_path="embrace/videos/$1/scene_$2/"$3:$4"/$experiment_name/masks/frame_0001.png"
-cp $stretched_video_path "ProPainter/inputs/video_completion/stretched.mp4"
-cp $mask_path "ProPainter/inputs/video_completion/frame0001.png"
-# TODO: we can change the mask at each frame, and set masks to alternate the block they keep so that each block has more references.
-cd ProPainter
-python inference_propainter.py \
-    --video inputs/video_completion/stretched.mp4 \
-    --mask inputs/video_completion/frame0001.png \
-    --neighbor_length $9 \
-    --ref_stride ${10} \
-    --subvideo_length ${11}
+# # inpaint video
+# cd
+# stretched_video_path="embrace/videos/$1/scene_$2/"$3:$4"/$experiment_name/stretched.mp4"
+# mask_path="embrace/videos/$1/scene_$2/"$3:$4"/$experiment_name/masks/frame_0001.png"
+# cp $stretched_video_path "ProPainter/inputs/video_completion/stretched.mp4"
+# cp $mask_path "ProPainter/inputs/video_completion/frame0001.png"
+# # TODO: we can change the mask at each frame, and set masks to alternate the block they keep so that each block has more references.
+# cd ProPainter
+# python inference_propainter.py \
+#     --video inputs/video_completion/stretched.mp4 \
+#     --mask inputs/video_completion/frame0001.png \
+#     --neighbor_length $9 \
+#     --ref_stride ${10} \
+#     --subvideo_length ${11}
 
-# move inpainted to experiment folder
-cd
-mv -f "ProPainter/results/stretched/inpaint_out.mp4" "embrace/videos/$1/scene_$2/"$3:$4"/$experiment_name/nei_${9}_ref_${10}_sub_${11}.mp4"
+# # move inpainted to experiment folder
+# cd
+# mv -f "ProPainter/results/stretched/inpaint_out.mp4" "embrace/videos/$1/scene_$2/"$3:$4"/$experiment_name/nei_${9}_ref_${10}_sub_${11}.mp4"
 
-cd embrace
+# cd embrace
 
-# quality degradation of inpainted video
-reference_video_path="videos/$1/scene_$2/"$3:$4"/original.mp4"
-distorted_video_path="videos/$1/scene_$2/"$3:$4"/$experiment_name/nei_${9}_ref_${10}_sub_${11}.mp4"
-reference_target_path="videos/$1/scene_$2/"$3:$4"/reference.yuv"
-distorted_target_path="videos/$1/scene_$2/"$3:$4"/distorted.yuv"
-# encode inpainted for vmaf, and save it in embrace folder renamed based on its parameters configuration
-ffmpeg -i $reference_video_path -f rawvideo -pix_fmt yuv420p $reference_target_path
-ffmpeg -i $distorted_video_path -f rawvideo -pix_fmt yuv420p $distorted_target_path
-# calculate quality degradation and save it as csv
-vmafossexec yuv420p $3 $4 $reference_target_path $distorted_target_path /home/shared/athena/vmaf/model/vmaf_v0.6.1.pkl \
-    --log videos/$1/scene_$2/"$3:$4"/$experiment_name/nei_${9}_ref_${10}_sub_${11}_inpainted.csv\
-    --log-fmt csv --psnr --ssim --ms-ssim
-# delete yuv files
-rm "videos/$1/scene_$2/"$3:$4"/reference.yuv"
-rm "videos/$1/scene_$2/"$3:$4"/distorted.yuv"
+# # quality degradation of inpainted video
+# reference_video_path="videos/$1/scene_$2/"$3:$4"/original.mp4"
+# distorted_video_path="videos/$1/scene_$2/"$3:$4"/$experiment_name/nei_${9}_ref_${10}_sub_${11}.mp4"
+# reference_target_path="videos/$1/scene_$2/"$3:$4"/reference.yuv"
+# distorted_target_path="videos/$1/scene_$2/"$3:$4"/distorted.yuv"
+# # encode inpainted for vmaf, and save it in embrace folder renamed based on its parameters configuration
+# ffmpeg -i $reference_video_path -f rawvideo -pix_fmt yuv420p $reference_target_path
+# ffmpeg -i $distorted_video_path -f rawvideo -pix_fmt yuv420p $distorted_target_path
+# # calculate quality degradation and save it as csv
+# vmafossexec yuv420p $3 $4 $reference_target_path $distorted_target_path /home/shared/athena/vmaf/model/vmaf_v0.6.1.pkl \
+#     --log videos/$1/scene_$2/"$3:$4"/$experiment_name/nei_${9}_ref_${10}_sub_${11}_inpainted.csv\
+#     --log-fmt csv --psnr --ssim --ms-ssim
+# # delete yuv files
+# rm "videos/$1/scene_$2/"$3:$4"/reference.yuv"
+# rm "videos/$1/scene_$2/"$3:$4"/distorted.yuv"
 
-# quality degradation of original video
-reference_video_path="videos/$1/scene_$2/"$3:$4"/original.mp4"
-distorted_video_path="videos/$1/scene_$2/"$3:$4"/$experiment_name/original.mp4"
-reference_target_path="videos/$1/scene_$2/"$3:$4"/reference.yuv"
-distorted_target_path="videos/$1/scene_$2/"$3:$4"/distorted.yuv"
-# encode inpainted for vmaf, and save it in embrace folder renamed based on its parameters configuration
-ffmpeg -i $reference_video_path -f rawvideo -pix_fmt yuv420p $reference_target_path
-ffmpeg -i $distorted_video_path -f rawvideo -pix_fmt yuv420p $distorted_target_path
-# calculate quality degradation and save it as csv
-vmafossexec yuv420p $3 $4 $reference_target_path $distorted_target_path /home/shared/athena/vmaf/model/vmaf_v0.6.1.pkl \
-    --log videos/$1/scene_$2/"$3:$4"/$experiment_name/nei_${9}_ref_${10}_sub_${11}_original.csv\
-    --log-fmt csv --psnr --ssim --ms-ssim
-# delete yuv files
-rm "videos/$1/scene_$2/"$3:$4"/reference.yuv"
-rm "videos/$1/scene_$2/"$3:$4"/distorted.yuv"
+# # quality degradation of original video
+# reference_video_path="videos/$1/scene_$2/"$3:$4"/original.mp4"
+# distorted_video_path="videos/$1/scene_$2/"$3:$4"/$experiment_name/original.mp4"
+# reference_target_path="videos/$1/scene_$2/"$3:$4"/reference.yuv"
+# distorted_target_path="videos/$1/scene_$2/"$3:$4"/distorted.yuv"
+# # encode inpainted for vmaf, and save it in embrace folder renamed based on its parameters configuration
+# ffmpeg -i $reference_video_path -f rawvideo -pix_fmt yuv420p $reference_target_path
+# ffmpeg -i $distorted_video_path -f rawvideo -pix_fmt yuv420p $distorted_target_path
+# # calculate quality degradation and save it as csv
+# vmafossexec yuv420p $3 $4 $reference_target_path $distorted_target_path /home/shared/athena/vmaf/model/vmaf_v0.6.1.pkl \
+#     --log videos/$1/scene_$2/"$3:$4"/$experiment_name/nei_${9}_ref_${10}_sub_${11}_original.csv\
+#     --log-fmt csv --psnr --ssim --ms-ssim
+# # delete yuv files
+# rm "videos/$1/scene_$2/"$3:$4"/reference.yuv"
+# rm "videos/$1/scene_$2/"$3:$4"/distorted.yuv"
 
-# run metrics script
-python metrics.py
+# # run metrics script
+# python metrics.py
 
-# save storage when running many experiments by deleting files and folders that will not be needed anymore
-# delete shrunk folder
-rm -r "videos/$1/scene_$2/"$3:$4"/$experiment_name/shrunk"
-# delete stretched folder
-rm -r "videos/$1/scene_$2/"$3:$4"/$experiment_name/stretched"
+# # save storage when running many experiments by deleting files and folders that will not be needed anymore
+# # delete shrunk folder
+# rm -r "videos/$1/scene_$2/"$3:$4"/$experiment_name/shrunk"
+# # delete stretched folder
+# rm -r "videos/$1/scene_$2/"$3:$4"/$experiment_name/stretched"
--- split_video_into_scenes.py
+++ split_video_into_scenes.py
@@ -1,10 +1,11 @@
 import os
-import shutil
 import subprocess
 import sys
 import cv2
+import numpy as np
+from skimage.metrics import structural_similarity as ssim
 
-def split_video_into_scenes(video_file: str, threshold: int = 100, max_scenes: int = None) -> str:
+def split_video_into_scenes(video_file: str, threshold: int = 0.9, max_scenes: int = None) -> str:
     
     # Extract folder path and create the folder if it doesn't exist, or exit if it already exists
     folder_path, _ = video_file.rsplit('.', 1)
@@ -27,20 +28,44 @@ def split_video_into_scenes(video_file: str, threshold: int = 100, max_scenes: i
     # Parse the video frame by frame
     while cap.isOpened():
         ret, frame = cap.read()
+        # Resize frame to speed up computation
+        height, width = frame.shape[:2]
+        aspect_ratio = height / width
+        new_height = int(640 * aspect_ratio)
+        frame = cv2.resize(frame, (640, new_height))
         if not ret:
             break
         if prev_frame is not None:
-            # Calculate absolute difference between frames
-            diff = cv2.absdiff(prev_frame, frame)
-            mean_diff = diff.mean()
-            # Detect scene changes based on mean difference exceeding the threshold
-            if mean_diff > threshold:
+
+            # # Calculate histogram correlation between frames
+            # hist = cv2.calcHist([frame], [0], None, [256], [0, 256])
+            # prev_hist = cv2.calcHist([prev_frame], [0], None, [256], [0, 256])
+            # score = cv2.compareHist(hist, prev_hist, cv2.HISTCMP_CORREL)
+            # if (score < threshold) and (cap.get(cv2.CAP_PROP_POS_MSEC) - scene_start > 2000):
+
+            # Calculate SSIM between frames
+            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray_prev_frame = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+            score, _ = ssim(gray_frame, gray_prev_frame, full=True)
+            if (score < threshold) and (cap.get(cv2.CAP_PROP_POS_MSEC) - scene_start > 2000):
+
+            # # Calculate optical flow between previous and current frame
+            # gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # gray_prev_frame = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+            # flow = cv2.calcOpticalFlowFarneback(gray_prev_frame, gray_frame, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            # mag, _ = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+            # score = np.mean(mag)
+            # # bring threshold to appropriate order of magnitude
+            # threshold *= 10
+            # if (score > threshold) and (cap.get(cv2.CAP_PROP_POS_MSEC) - scene_start > 2000):
+
+                print(score)
                 # Get times of scene start and end, and save new video between those times
                 scene_end = cap.get(cv2.CAP_PROP_POS_MSEC)
                 # Save scene using ffmpeg
                 output_file = os.path.join(folder_path, f"scene_{scene_number}.mp4")
                 # Silently execute ffmpeg command
-                ffmpeg_command = ["ffmpeg", "-i", video_file, "-ss", f"{scene_start/1000}", "-to", f"{scene_end/1000}", "-c:v", 'libx265', "-crf", "0", "-pix_fmt", "yuv420p", "-an", output_file]
+                ffmpeg_command = ["ffmpeg", "-i", video_file, "-ss", f"{scene_start/1000}", "-to", f"{scene_end/1000}", "-c:v", 'libx265', "-crf", "20", "-pix_fmt", "yuv420p", "-an", output_file]
                 with open(os.devnull, 'w') as null_file:
                     subprocess.run(ffmpeg_command, stdout=null_file, stderr=null_file)
                 scene_start = scene_end
@@ -58,6 +83,7 @@ def split_video_into_scenes(video_file: str, threshold: int = 100, max_scenes: i
 if __name__ == "__main__":
     split_video_into_scenes(
         video_file=sys.argv[1], 
-        threshold=int(sys.argv[2]), 
+        threshold=float(sys.argv[2]), 
         max_scenes=int(sys.argv[3])
     )
+
